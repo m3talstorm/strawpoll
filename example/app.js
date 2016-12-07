c7ac172e8fae96cf7dc82e520bfbd8cde9a88bdf
Stuart Bannerman <metalheadstorm@gmail.com>
2016-12-07 21:55:23 +0000

    Add option name to option id mapper, started work on voting

--- example/app.js
+++ example/app.js
@@ -19,10 +19,30 @@ poll.then((response) => {
     console.log(error)
 })
 
-var poll2 = strawpoll.get(11822190)
 
-poll2.then((response) => {
+const TEST_POLL = 11823127
+
+var got = strawpoll.get(TEST_POLL)
+
+got.then((response) => {
     console.log(response)
 }).catch((error) => {
     console.log(error)
+})
+
+const mapping = strawpoll.options(TEST_POLL)
+
+mapping.then((options) => {
+    console.log("OPTIONS", options)
+}).catch((error) => {
+    console.log("OPTIONS ERROR", error)
+})
+
+
+var voted = strawpoll.vote(TEST_POLL, 117430681)
+
+voted.then((response) => {
+    console.log("VOTED", response)
+}).catch((error) => {
+    console.log("VOTED ERROR", error)
 })
